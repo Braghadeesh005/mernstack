71a3b65e469a3e403194ad37b1114fdd716bef08
Braghadeesh R A <114174919+Braghadeesh005@users.noreply.github.com>
2023-01-04 20:22:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,8 +24,9 @@ app.get("/signin",(req,res) => {
 if(process.env.NODE_ENV == "production"){
     app.use(express.static("client/build"));
 }
-*/
 app.use(express.static(path.join(__dirname, "./client/build")));
+*/
+app.use(express.static("client/build"));
 app.get("*",function(req,res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 })
